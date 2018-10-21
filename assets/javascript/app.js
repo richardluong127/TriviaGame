1bb476b0e0c0bac86bac35d5b7828c70d531c4e1
richardluong127 <richardluong127@gmail.com>
2018-10-20 22:24:14 -0700

    correct answer function cleaned

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,30 +1,35 @@
 $(document).ready(function () {
 
-    var correctAnswer;
-    var incorrectAnswer;
-    var qTRQ = ["Forevermore", "Nevermore", "Never ever", "Forever and a Day"];
-    var kBQ = ["81 points", "99 points", "62 points", "76 points"];
-    var disneyQ = ["Darwing Duck", "Mike Meyers", "Spongebob Squarepants", "Jack Skellington"];
-    var musicalQ = ["Chicago", "Moulin Rouge", "Final Fantasy", "Todd Sweeney"];
-    var pokerQ = ["Flush", "Blackjack", "Two Pair", "Three of a Kind"];
+ 
+    // var qTRQ = ["Forevermore", "Nevermore", "Never ever", "Forever and a Day"];
+    // var kBQ = ["81 points", "99 points", "62 points", "76 points"];
+    // var disneyQ = ["Darwing Duck", "Mike Meyers", "Spongebob Squarepants", "Jack Skellington"];
+    // var musicalQ = ["Chicago", "Moulin Rouge", "Final Fantasy", "Todd Sweeney"];
+    // var pokerQ = ["Flush", "Blackjack", "Two Pair", "Three of a Kind"];
 
 
-    var questions = [qTRQ, kBQ, disneyQ, musicalQ, pokerQ];
-    var intervalId;
-    var timer = {};
-    var clickedAnswer;
-    var correctAnswer = qTRQ[1];
+    // var questions = [qTRQ, kBQ, disneyQ, musicalQ, pokerQ];
+    // var intervalId;
+    // var timer = {};
+    // var clickedAnswer;
+    // var correctAnswer = qTRQ[1];
 
 
+//hideStart =  $("#jumbostart").hide();
 
+var correctAnswer= 0;
+var incorrectAnswer =0;
 
     $("#start").on("click", start);
 
+
     $("p").on("click", function () {
-        //console.log(this);
-        
-            if (event.target.id === "b1") {
-                console.log("perfect");
+            if ((event.target.id === "b1") || (event.target.id === "d2") || (event.target.id === "a3")) {
+                correctAnswer++;
+                console.log(correctAnswer);
+            }else {
+                incorrectAnswer++;
+                console.log(incorrectAnswer);
             }
         }
     );
